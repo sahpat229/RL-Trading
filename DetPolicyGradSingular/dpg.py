3c2932062d6dcebbc961bcb4ca509bc86562d56f
Sahil Patel <sahpat229@gmail.com>
2018-02-21 12:36:26 -0500

    Added in Agg backend for figure saving

--- DetPolicyGradSingular/dpg.py
+++ DetPolicyGradSingular/dpg.py
@@ -1,4 +1,7 @@
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+
 import numpy as np 
 import random
 import tensorflow as tf 
--- DetPolicyGradSingular/dpg_test.py
+++ DetPolicyGradSingular/dpg_test.py
@@ -1,4 +1,6 @@
-import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('Agg')
+
 import numpy as np
 import tensorflow as tf
 
